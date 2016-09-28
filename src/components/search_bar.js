5caaa566a2dc64140c3a26ff6d2014bdd8fbf13c
trebek1 <trebek08@gmail.com>
2016-09-28 16:37:56 -0700

    search bar with react es6

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,34 @@
-import React from 'react'; 
+import React, {Component} from 'react'; 
 
+// class based can have added functionality 
+// only class based components have state, functional do not 
 
-const SearchBar = () => {
-	return <input />
+class SearchBar extends Component {
+// new instance created -> call constructor 
+	constructor(props){
+
+		// call it on Component (parent)
+		super(props);
+
+		this.state = {term: ''};
+		this.onInputChange = this.onInputChange.bind(this); 
+	}
+	
+	onInputChange(event){
+		console.log(event.target.value)
+		this.setState({term: event.target.value})
+	}
+
+	render () {
+		return <div><input onChange={this.onInputChange} />
+		Value of input : {this.state.term}
+		</div>
+	}
 }
+
+//functional component, no added functionality dumb component
+// const SearchBar = () => {
+// 	return <input />
+// }
 
 export default SearchBar; 
