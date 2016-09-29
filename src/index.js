34c54348dc29df562aab618262a0043964814227
trebek1 <trebek08@gmail.com>
2016-09-28 17:44:53 -0700

    added show of first video

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import reducers from './reducers';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAToT3fCuAT3_6WC5jxRjn_fd6pJ4Hv7SY';
 
@@ -33,6 +34,7 @@ class App extends Component {
 		return(
 			<div>
 				<SearchBar/>
+				<VideoDetail video={this.state.videos[0]}/>
 				<VideoList videos = {this.state.videos} />
 			</div>	
 		) 
